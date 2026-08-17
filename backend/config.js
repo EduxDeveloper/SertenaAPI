48777a83a3d5ebc2ab687069b71c94aad67938d5
ItsChamboDeveloper <imchambo503@gmail.com>
2026-08-16 20:36:16 -0900

    Se arreglo filtracion de base

--- backend/config.js
+++ backend/config.js
@@ -15,6 +15,9 @@ export const config = {
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_API_SECRET
+    },
+    db:{
+        uri: process.env.DB_URI
     }
 };
 
